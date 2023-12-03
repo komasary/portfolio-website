076809dcc0812320a646acc8cad95d0ce95a4af0
Komas <Komasary@gmail.com>
2023-12-03 16:03:03 -0500

    Second push

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,7 +33,7 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:komasary97@gmail.com">
+        <a className="underline" href="mailto:komasary@gmail.com">
           komasary97@gmail.com
         </a>{" "}
         or through this form.
